101e15a2020733bb29575fc3f1f13a74f7772961
MinahilDeveloper <minahil.nasir258@gmail.com>
2025-04-27 21:06:05 +0500

    Add files via upload

---
+++ Shopping mall/app.js
@@ -0,0 +1,6 @@
+let show= alert("WELCOME TO OUR SHOP");
+let num1= prompt("Which item do you want to buy?");
+let num2= prompt("Enter the price of the item:");
+let num3= prompt("Enter quantity of the item:");
+let total=num2*num3;
+alert("You want to buy " +num1+ " having price "+num2+ " in quantity "+num3+ " and the subtotal is "+total);
